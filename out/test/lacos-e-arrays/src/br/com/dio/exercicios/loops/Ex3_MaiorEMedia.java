f38da6a31fea3998f232fceeefd3727913402de9
Cicero Romao <cicerotransformice@gmail.com>
2022-05-04 11:13:36 -0300

    exe3 concluido

--- out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -31,4 +31,33 @@ public class Ex3_MaiorEMedia {
 //        System.out.println("Média: " + (soma/5));
 //
 //    }
+
+    /*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números.
+*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+
+        do {
+
+            System.out.println("Numero: ");
+            numero = sc.nextInt();
+            soma += numero;
+
+            if(numero > maior) maior = numero;
+            count += 1;
+        } while(count < 5);
+        
+        System.out.println("Maior: " + maior);
+        System.out.println("Média: " + (soma/5));
+
+    }
 }
