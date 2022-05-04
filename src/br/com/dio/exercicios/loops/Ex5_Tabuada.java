de28c680a70a7abacc0504979e1771dc1c94114a
Cicero Romao <cicerotransformice@gmail.com>
2022-05-04 14:21:54 -0300

    exe5 concuido

--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -26,5 +26,37 @@ public class Ex5_Tabuada {
 //            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
 //        }
 //    }
+/*
+Desenvolva um gerador de tabuada,
+capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual numero ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
 
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+*/
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int sair = 0;
+
+    while(sair == 0) {
+        System.out.println("Tabuada: ");
+        int tabuada = scanner.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(tabuada + "*" + i + " = " + (tabuada*i));
+        }
+
+        System.out.println("deseja sair? 0 continua 1 sair");
+        sair = scanner.nextInt();
+    }
+
+    }
+
+    public static void operacao(int numero, String operacao) {
+
+    }
 }
