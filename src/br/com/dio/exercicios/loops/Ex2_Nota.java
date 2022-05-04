d84bfea457118ccf4b1ac73cd4398f35f00b1a84
Cicero Romao <cicerotransformice@gmail.com>
2022-05-04 10:50:18 -0300

    comment all exe

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -9,19 +9,19 @@ e continue pedindo
 até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int nota;
-
-        System.out.println("Nota: ");
-        nota = scan.nextInt();
-
-        while(nota < 0 | nota > 10) {
-            System.out.println("Nota Inválida! Digite novamente: ");
-            nota = scan.nextInt();
-        }
-
-    }
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int nota;
+//
+//        System.out.println("Nota: ");
+//        nota = scan.nextInt();
+//
+//        while(nota < 0 | nota > 10) {
+//            System.out.println("Nota Inválida! Digite novamente: ");
+//            nota = scan.nextInt();
+//        }
+//
+//    }
 
 }
--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -15,16 +15,16 @@ Tabuada de 5:
 5 X 10 = 50
 */
 public class Ex5_Tabuada {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Tabuada: ");
-        int tabuada = scan.nextInt();
-
-        System.out.println("Tabuada de " + tabuada);
-        for(int i = 1; i <= 10; i = i + 1) {
-            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
-        }
-    }
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Tabuada: ");
+//        int tabuada = scan.nextInt();
+//
+//        System.out.println("Tabuada de " + tabuada);
+//        for(int i = 1; i <= 10; i = i + 1) {
+//            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+//        }
+//    }
 
 }
